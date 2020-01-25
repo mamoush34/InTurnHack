c6062db2e504c50c72cfe3ef59cf7e7a69834d9e
Sam Wilkins <samwilkins333@gmail.com>
2020-01-25 15:48:00 -0500

    added database and chrome extension improvements

---
+++ chrome_extension/build/precache-manifest.e2419631e26d104fce9dad0b95af20b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9d875dad5440fb22d96f67712878f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c145d1b70c1280cb6959",
+    "url": "/static/css/main.57d86ec5.chunk.css"
+  },
+  {
+    "revision": "14370d6e1763873cf681",
+    "url": "/static/js/2.434cb306.chunk.js"
+  },
+  {
+    "revision": "c145d1b70c1280cb6959",
+    "url": "/static/js/main.0800e55b.chunk.js"
+  },
+  {
+    "revision": "6d808ffb3490c9abf6cf",
+    "url": "/static/js/runtime-main.ea70d6da.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- web_application/src/server/index.ts
+++ web_application/src/server/index.ts
@@ -1,8 +1,14 @@
 import * as express from "express";
 import { resolve } from "path";
 import * as cors from "cors";
+import * as bodyParser from "body-parser";
+import { Database } from "./database";
 
 const port = 1050;
+const database = {
+    name: "InTurn",
+    userCollection: "users"
+}
 
 const static_path = resolve(__dirname, "../../static");
 const content_path = resolve(__dirname, "../../src/index.html");
@@ -10,26 +16,33 @@ const content_path = resolve(__dirname, "../../src/index.html");
 const server = express();
 
 server.use(cors());
-server.use(express.static(static_path));
-server.use((req, _res, next) => {
-    console.log(req.originalUrl);
-    next();
-});
+server.use(bodyParser.json({ limit: "10mb" }));
+server.use(bodyParser.urlencoded({ extended: true }));
 
-console.log(`Server listening on port ${port}...`);
+server.use(express.static(static_path));
 
 server.get("/", (_req, res) => res.redirect("/logo"));
 server.get("/logo", (_req, res) => {
     res.sendFile(content_path);
 });
 
-server.get("/currentUser", (_req, res) => {
-    res.send({
-        first_name: "Samuel",
-        last_name: "Wilkins",
-        email: "samuel_wilkins@brown.edu",
-        phone: "4158239674"
+server.post("/inquireUser", async (req, res) => { 
+    const { body: { email } } = req;
+    (await Database.getOrCreateCollection(database.userCollection)).findOne({ email }, (error, result) => {
+        res.send(error ? undefined : result);
     });
 });
 
-server.listen(port);
+(async () => {
+    const { userCollection, name } = database;
+    await Database.connect(name);
+    if (!(await Database.existsCollection(userCollection))) {
+        await Database.insert(userCollection, {
+            first_name: "Samuel",
+            last_name: "Wilkins",
+            email: "samuel_wilkins@brown.edu",
+            phone: "4158239674"
+        });    
+    }
+    server.listen(port, () => console.log(`Server listening on port ${port}...`));
+})();
