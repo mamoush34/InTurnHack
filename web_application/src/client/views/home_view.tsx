108e4777803cfd6804fa2af612e132a783b1daa5
Mohammad Amoush <47069173+mamoush34@users.noreply.github.com>
2020-01-26 09:18:16 -0500

    font-awesome icon added

--- web_application/src/client/views/home_view.tsx
+++ web_application/src/client/views/home_view.tsx
@@ -1,12 +1,18 @@
 
 import * as React from "react";
 import "./home_view.scss";
-import { observable, action } from "mobx";
+import { observable, action, computed, computed } from "mobx";
 import { observer } from "mobx-react";
 import { Job } from "../models/Job";
 import { ApplicationRec } from "./application_rec";
 import AddJobPage from "./add_job_page";
 import { Server } from "../utilities";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faIdBadge } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+library.add(faIdBadge);
+
 
 export interface HomeViewProps {
     background: string;
@@ -37,17 +43,13 @@ export default class HomeView extends React.Component<HomeViewProps> {
         this.openCreation = false;
     }
 
-    renderAddition = () => {
+    @computed
+    private get renderPage() {
         if (this.openCreation) {
             return <AddJobPage close={this.close} addJob={this.addJob} />
-        }
-        return (null)
-    }
-
-    render() {
-        const { background } = this.props;
-        return (
-            <div
+        } else {
+            const { background } = this.props;
+            return <div
                 className="container"
                 style={{ background }}
             >
@@ -64,15 +66,19 @@ export default class HomeView extends React.Component<HomeViewProps> {
                         }}
                     />
                 </div>
-                <a className="registration" id="register" onClick={() => {
+                <a className="registration" id="register" title="Add Job!" onClick={() => {
                     this.openCreation = true;
                 }}>
-                    Add Job
+                    <FontAwesomeIcon icon={faIdBadge} size={"3x"} ></FontAwesomeIcon>
                 </a>
-                {this.renderAddition()}
                 {this.jobs.map(job => (<ApplicationRec listedJob={job} ></ApplicationRec>))}
             </div>
-        );
+        }
+        
+    }
+
+    render() {
+        return this.renderPage;
     }
 
 }
